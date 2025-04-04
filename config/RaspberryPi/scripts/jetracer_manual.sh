7053bce2a650cd2531ec59d580b1aad476e266e1
Axel chabrerie <114242486+Axel-ex@users.noreply.github.com>
2025-04-04 14:08:05 +0100

    update config with new names (jetracer)

--- config/RaspberryPi/scripts/jetracer_manual.sh
+++ config/RaspberryPi/scripts/jetracer_manual.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker run \
-	--name piracer \
+	--name jetracer \
 	--rm \
 	--net=host \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
@@ -18,4 +18,4 @@ docker run \
 	-v /run/udev:/run/udev:ro \
 	--group-add video \
 	-e LAUNCH_FILE="JetRacer_manual_launch.py" \
-	piracer_image
+	jetracer_image
